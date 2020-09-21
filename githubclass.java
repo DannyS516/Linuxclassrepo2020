1a1abc57f26d99f5c8377b9d43401b129569f93a
Danny Sepulveda <danny.sepulveda@comcast.net>
2020-09-21 04:35:09 +0000

    I am taking CS407 because

--- githubclass.java
+++ githubclass.java
@@ -1,6 +1,6 @@
 public class githubclass{ 
 		public static void main(String[] args){
-			System.out.println("My favorite programming language is python");
+			System.out.println("I am taking CS407 because this class is interesting");
 		}
 }
 
