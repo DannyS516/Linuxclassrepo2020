ce5d4c753e8f860ca8102fb0e5c96d2ee825a7be
Danny Sepulveda <danny.sepulveda@comcast.net>
2020-09-21 04:36:45 +0000

    My favorite food is

--- githubclass.java
+++ githubclass.java
@@ -1,6 +1,6 @@
 public class githubclass{ 
 		public static void main(String[] args){
-			System.out.println("I am taking CS407 because this class is interesting");
+			System.out.println("My favorite food is Chinese Food");
 		}
 }
 
